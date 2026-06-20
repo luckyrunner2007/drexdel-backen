74337e4202fb846384e686171726fe25cd310c1e
luckyrunner2007 <ishimwebega7@gmail.com>
2026-06-20 23:25:49 +0200

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#FAFE'}}>
+      <Text style={{color:'#7B2CBF', fontSize:24, fontWeight:'700'}}>
+        DREXDEL LOADED ✅
+      </Text>
+    </View>
+  );
+}
